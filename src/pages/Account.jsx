b178cf6cfb33a2735d8c5ca928c95052e2fe1fe8
Cristian Cebotari <cebotaricristian92@gmail.com>
2024-08-09 20:54:15 +0300

    v2.5.0

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,19 +1,32 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
+import ButtonText from "../ui/ButtonText";
+import { useMoveBack } from "../hooks/useMoveBack";
+import styled from "styled-components";
+
+const FlexRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 function Account() {
+  const moveBack = useMoveBack();
   return (
     <>
-      <Heading as="h1">Update your account</Heading>
+      <FlexRow>
+        <Heading as="h1">Update your account</Heading>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+      </FlexRow>
 
       <Row>
-        <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
+        <Heading as="h3">Update user data</Heading> <UpdateUserDataForm />
       </Row>
 
       <Row>
         <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
+        <UpdatePasswordForm />
       </Row>
     </>
   );
